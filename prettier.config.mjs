0adf75a6a599b8b98dd83562a522d69edb0e1f31
kimdonggu42 <kdk3280@undermilli.com>
2025-06-17 10:02:48 +0900

    chore: 프로젝트 초기 설정 및 보일러플레이트 정리

---
+++ prettier.config.mjs
@@ -0,0 +1,9 @@
+const prettierConfig = {
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 100,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss'],
+};
+
+export default prettierConfig;
