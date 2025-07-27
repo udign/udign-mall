8e14dcb192f2556d0b2c598f4c41d77ebc7d06fc
ivxvi12 <iam@undermilli.com>
2025-07-27 15:50:32 +0900

    fix: rm logo in background images

--- src/app/shop/reset-password/page.tsx
+++ src/app/shop/reset-password/page.tsx
@@ -69,22 +69,6 @@ function ResetPasswordContent() {
         backgroundColor: '#1a2332',
       }}
     >
-      <div className='relative z-10'>
-        <div className='px-6 py-5 sm:px-10'>
-          <div className='flex items-center'>
-            <Link href={ROUTES.HOME} className='flex items-center'>
-              <Image
-                src='/images/udign-white.png'
-                alt='UDIGN'
-                width={100}
-                height={40}
-                className='h-auto'
-              />
-            </Link>
-          </div>
-        </div>
-      </div>
-
       <div className='relative z-10 mt-10 flex flex-1 justify-center p-4'>
         <div className='w-full max-w-lg'>
           <div className='rounded-lg border border-gray-600 bg-black/80 p-8 backdrop-blur-sm'>
