5ebc9eb51ce570c81da17b486cfb2abfcd2aeb4a
kimdonggu42 <kdk3280@undermilli.com>
2025-07-29 13:46:34 +0900

    feat: 비밀번호 찾기 페이지 국제화 적용

--- src/app/[lang]/shop/forgot-password/page.tsx
+++ src/app/[lang]/shop/forgot-password/page.tsx
@@ -1,8 +1,15 @@
-'use client';
-
+import { getDictionary } from '@/lib/dictionaries';
+import { Locale } from '../../../../../i18n.config';
 import ForgotPasswordForm from '@/components/ForgotPasswordForm';
 
-export default function ForgotPasswordPage() {
+interface ForgotPasswordPageProps {
+  params: Promise<{ lang: Locale }>;
+}
+
+export default async function ForgotPasswordPage({ params }: ForgotPasswordPageProps) {
+  const { lang } = await params;
+  const dictionary = await getDictionary(lang);
+
   return (
     <div
       className='flex min-h-screen flex-col bg-cover bg-fixed bg-center bg-no-repeat'
@@ -13,7 +20,7 @@ export default function ForgotPasswordPage() {
     >
       <div className='relative z-10 mt-10 flex flex-1 justify-center p-4'>
         <div className='w-full max-w-lg'>
-          <ForgotPasswordForm />
+          <ForgotPasswordForm dictionary={dictionary} />
         </div>
       </div>
     </div>
