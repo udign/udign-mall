9bc90c1b6b5432b08ae1249e81849a37fa4e1d37
udign <udign0401@naver.com>
2026-02-05 17:15:20 +0900

    Update layout.tsx

--- src/app/[lang]/shop/layout.tsx
+++ src/app/[lang]/shop/layout.tsx
@@ -1,4 +1,4 @@
-import { } from 'react';
+import React from 'react';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import PopupDisplay from '@/components/PopupDisplay';
