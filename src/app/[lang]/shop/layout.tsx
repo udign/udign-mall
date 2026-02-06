10a2ae330020ac4762286e23f01840ca2cab9b50
udign <udign0401@naver.com>
2026-02-06 17:20:17 +0900

    Update layout.tsx

--- src/app/[lang]/shop/layout.tsx
+++ src/app/[lang]/shop/layout.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import PopupDisplay from '@/components/PopupDisplay';
 import { getDictionary } from '@/lib/dictionaries';
-import { Locale } from "../../../../i18n.config";
+type Locale = "en" | "ja" | "ko" | "zh";
 
 interface CategoryLayoutProps {
   children: React.ReactNode;
@@ -13,7 +13,7 @@ interface CategoryLayoutProps {
 export default async function CategoryLayout({ children, params }: CategoryLayoutProps) {
   // params를 await로 받아옵니다.
   const { lang } = await params;
-  const dictionary = await getDictionary(lang);
+  const dictionary = await getDictionary(lang as Locale);
 
   return (
     <>
